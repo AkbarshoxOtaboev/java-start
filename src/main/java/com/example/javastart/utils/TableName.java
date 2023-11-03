aca0819ca891cf252ae4e83958b6d1b401425326
AkbarshoxOtaboev <otaboevakbar96@gmail.com>
2023-11-03 17:09:06 +0500

    TK - 001 is completed

---
+++ src/main/java/com/example/javastart/utils/TableName.java
@@ -0,0 +1,9 @@
+package com.example.javastart.utils;
+
+public interface TableName {
+    String USERS = "users";
+    String USERPROFILE= "user_profile";
+    String POSTS = "posts";
+    String COMMENTS = "comments";
+    String FAVOURITES = "favourites";
+}
